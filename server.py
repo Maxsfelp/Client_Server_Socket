2def448c7c44c3fc07cff8cd51783989b4fbb636
Maxsfelp <felipemaxsuel74@gmail.com>
2021-03-23 22:04:15 -0300

    Recebendo e enviando dados para o client

--- server.py
+++ server.py
@@ -5,26 +5,36 @@ sexo = []
 idade = []
 dados = []
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as UDPServer:
-    UDPServer.bind((HOST, PORT))
-    UDPServer.listen()
-    conn, addr = UDPServer.accept()
-    
-    with conn:
-        
-        while True:
-            data = conn.recv(4096)
-            if not data:
-                break;
-            data.decode('utf-8')
-            data = eval(data)
-            if data[0] == 'SELECT*':
-                if len(dados) != 0:
-                    conn.sendall(dados.encode())
-            else:
-                nome.append(data[1])
-                sexo.append(data[2])
-                idade.append(data[3])
-                dados = [nome, sexo, idade]
-                dados = str(dados)
-                conn.sendall(dados.encode())
+def sendData(conn, dados):
+    if len(dados) != 0:
+        dados = str(dados)
+        conn.sendall(dados.encode())
+
+def saveData(data):
+    nome.append(data[1])
+    sexo.append(data[2])
+    idade.append(data[3])
+    dados = [nome, sexo, idade]
+    return dados
+
+def recvData(conn, dados):
+    data = conn.recv(1024)
+    if data:
+        data.decode('utf-8')
+        data = eval(data)
+        if data[0] == 'SELECT*':
+            sendData(conn, dados)
+        elif data[0] == 'INSERT':
+            dados = saveData(data)
+    return dados
+
+HOST = '127.0.0.1'
+PORT = 65432
+UDPServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+UDPServer.bind((HOST, PORT))
+UDPServer.listen()
+conn, addr = UDPServer.accept()
+while 1:
+    dados = recvData(conn, dados)
+conn.close()
+UDPServer.close()
